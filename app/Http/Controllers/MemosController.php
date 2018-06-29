9d01f3f76eee6cbaa90e38048031c81bbf410dea
yusuke.kimura <yusuke.a.kimura@rakuten.com>
2018-06-29 06:13:23 +0000

    URLで検索

--- app/Http/Controllers/MemosController.php
+++ app/Http/Controllers/MemosController.php
@@ -4,18 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Memo;
+use App\User;
 
 class MemosController extends Controller
 {
-    /**
-     * メモ一覧を表示する。
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
-        $memos = Memo::all();
-        return $memos;
-        return view('memos.index',compact('memos'));
+        $memos = Memo::searchTitle(request('word'))
+           ->get(); //結果を取得
+        return view('memos.index', compact('memos'));
     }
 }
--- app/Memo.php
+++ app/Memo.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Memo extends Model
 {
     protected $guarded = [];
+    
+    
+    public function scopeTagFilter($query, ?string $tag)
+    {
+        if (!is_null($tag)) {
+            return $query->where('tag', $tag);
+    }
+        return $query;
+    }
+    
+    public function scopeSearchTitle($query, ?string $word)
+    {
+        if (!is_null($word)) {
+            return $query->where('title', 'like', '%' . $word . '%');
+        }
+        return $query;
+    }
 }
+
+
